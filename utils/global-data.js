79cd1184dfe8eda5b4a3374f76d00ca67bb95e93
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-01-14 19:20:48 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'AJay Dhangar';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
