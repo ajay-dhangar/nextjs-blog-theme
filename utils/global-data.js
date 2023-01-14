b1c612f316f732ef695c68cd76188096382adca9
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-01-14 19:34:08 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'AJay Dhangar';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Well-ComeNext.js Blog';
+    : 'Next.js Blog';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
